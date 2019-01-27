8f34830f79a30d79cc64a44744c36d80b39e5954
adventuroussheep <jacob_haas_7@hotmail.com>
2019-01-27 09:06:10 -0800

    added base files and style

---
+++ assests/javascript/javascript.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBlDIHhIWlKMXoR7Hwm0klOScus8da7FXc",
+    authDomain: "train-schedule-2c9ba.firebaseapp.com",
+    databaseURL: "https://train-schedule-2c9ba.firebaseio.com",
+    projectId: "train-schedule-2c9ba",
+    storageBucket: "train-schedule-2c9ba.appspot.com",
+    messagingSenderId: "747766541073"
+  };
+  firebase.initializeApp(config);
